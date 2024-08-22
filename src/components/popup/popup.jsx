a8aba3ef275a36c3f2b5b8a85e16e3e28f860e95
Airyyy23 <wayangseno123@gmail.com>
2024-08-22 21:40:51 +0700

    p

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -10,7 +10,7 @@ const popup = ({ isOpen, onClose }) => {
       <div className='relative bg-zinc-900 rounded-lg shadow-xl max-w-5xl max-h-[80vh] overflow-y-auto z-10 custom-scrollbar scroll-smooth'>
         <img src={background} alt="image" className='w-[2000px]'/>
         <div className='p-5'>
-          <h1>title</h1>
+          <h1>Selamatt Ulang Tahunn yaaa</h1>
           <p>selamat yaa, kamu hari ini hari special kamu, umur kamu sekarang bertambah 1 tahun, kamu tau ga, kamu itu kuat, kamu hebat, kamu udah sejauh ini ngejalanin hari hari kamu, mulai dari kamu masi di rahim mamim sampe kamu udah sedewasa ini, makasi yaa kamu udah mau ngejalanin hari hari kamu bareng aku walaupun aku banyak bikin masalah, tetep sama aku ya? kalo ga sama kamu rasanya itu kurang soalnya wkwk, kamu seneng ngga pas hari special kamu ini kita masih bareng bareng begini? aku harap pas kamu ulang tahun, semua kemauan kamu bisa terwujud satu per satu, semoga kamu panjang umur, diberi kesehatan selalu, dimudahkan segala urusan kamu, kalo hari hari kamu ada masalah tetep semangat ya aku ada disini selalu buat kamu, i love u so much.</p>
         </div>
       </div>
